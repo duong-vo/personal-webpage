217c06d46acf44743983c3877b42b4702bcb6a99
duong-vo <vodh2@miamioh.edu>
2023-02-03 00:52:33 -0500

    start to fetch the github api for the portfolio

--- frontend/src/components/Box/index.js
+++ frontend/src/components/Box/index.js
@@ -1,11 +1,30 @@
 import './index.css'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Box = () => {
+    const [title, setTitle] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(`https://api.github.com/users/duong-vo/repos`)
+                .then(response => response.json())
+                .then(repositories => {
+                    const pinnedRepos = repositories.filter(repo => repo.isPinned);
+                    console.log(pinnedRepos);
+                })
+                .catch(error => console.error(error));
+            const json = await res.json();
+            setTitle(json);
+        };
+        fetchData();
+    }, []);
+
     return (
         <div class="box">
             <div class="pin-text">
-                <h3>Title</h3>
+                {title.map(item => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
                 <p>Description</p>
             </div>
         </div>
