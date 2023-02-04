9152ffbb70332738c73ddcb200c85689f205b20d
duong-vo <vodh2@miamioh.edu>
2023-02-03 23:25:44 -0500

    make fetching github pinned repos work

--- frontend/src/components/Box/index.js
+++ frontend/src/components/Box/index.js
@@ -3,32 +3,42 @@ import React, { useState, useEffect } from 'react';
 
 const Box = () => {
     const [title, setTitle] = useState([]);
-
+    // hard coded pinned ids
+    const pinnedIds = [564903106, 594309823, 560730978,589031379,591131178,541322654];
+    var pinnedRepos = []
     useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(`https://api.github.com/users/duong-vo/repos`)
-                .then(response => response.json())
-                .then(repositories => {
-                    const pinnedRepos = repositories.filter(repo => repo.isPinned);
-                    console.log(pinnedRepos);
-                })
-                .catch(error => console.error(error));
-            const json = await res.json();
-            setTitle(json);
+        const fetchData = () => {
+            const res = fetch(`https://api.github.com/users/duong-vo/repos`) // return a promise
+                                   .then(res => { return res.json()}) // also return a promise
+                                   .then(repos => {
+                                         pinnedRepos = [];
+                                        for (let i = 0; i < repos.length; i++) {
+                                            console.log(repos[i]);
+                                            if (pinnedIds.indexOf(repos[i].id) != -1) {
+                                                pinnedRepos.push(repos[i]);
+                                            }
+                                        }
+                                        console.log(pinnedRepos);
+                                        setTitle(pinnedRepos);
+                                    })
+            // const json = res.json();
+            // setTitle(json);
         };
         fetchData();
     }, []);
 
     return (
-        <div class="box">
-            <div class="pin-text">
-                {title.map(item => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-                <p>Description</p>
-            </div>
-        </div>
-    );
+        <div>
+            {title.map(item => (
+                <div class="box">
+                    <div class="pin-text">
+                        <li key={item.id}>{item.name}</li>
+                        <p>Description</p>
+                    </div>
+                </div>
+            ))}
+
+        </div>);
 }
 
 export default Box;
