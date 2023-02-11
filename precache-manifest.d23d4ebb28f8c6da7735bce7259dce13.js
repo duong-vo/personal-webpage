be86480b65f1d62415748c00571ca382d1b53d18
duong-vo <vodh2@miamioh.edu>
2023-02-10 22:04:41 -0500

    Updates

---
+++ precache-manifest.d23d4ebb28f8c6da7735bce7259dce13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8f4c4a84647291acf5d",
+    "url": "/static/css/main.330940ad.chunk.css"
+  },
+  {
+    "revision": "e8f4c4a84647291acf5d",
+    "url": "/static/js/main.e8f4c4a8.chunk.js"
+  },
+  {
+    "revision": "622dfb5a9e4f352f35ec",
+    "url": "/static/js/1.622dfb5a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4eb611c14707895f33e715f27094ff67",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-webpage/precache-manifest.5123a67273fca3e99c0ca2906d2677a0.js"
+  "/precache-manifest.d23d4ebb28f8c6da7735bce7259dce13.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/personal-webpage/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
